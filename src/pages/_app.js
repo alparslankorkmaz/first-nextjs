8733310c17d55f3ded0ae277e5667aef0a657e2d
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-05-05 14:35:49 +0100

    Revert "styles on production"
    This reverts commit b3d4a6427a5dcdde241d9252fc51cf91d16ab470.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
-import "../styles/globals.css";
-import "../styles/menu.css";
-import "../styles/slider.css";
+import "@/styles/globals.css";
+import "@/styles/menu.css";
+import "@/styles/slider.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
