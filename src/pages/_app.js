49c906888bbaccc8ff9212fbe8c1c84e0b1cd426
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-05-05 13:11:26 +0100

    styles fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,7 @@
+import "public/styles/globals.css";
+import "public/styles/menu.css";
+import "public/styles/slider.css";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
