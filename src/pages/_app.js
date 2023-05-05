688bd94e32447b6fdb7482f384b48e7f0bbf6fcf
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-05-05 14:35:49 +0100

    Revert "styles fix"
    This reverts commit 49c906888bbaccc8ff9212fbe8c1c84e0b1cd426.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,3 @@
-import "public/styles/globals.css";
-import "public/styles/menu.css";
-import "public/styles/slider.css";
-
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
